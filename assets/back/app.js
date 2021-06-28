fac8c77e03cb57494b8e1291aa17a8edd24ef10d
Moog404 <eangly.eng@gmail.com>
2021-06-28 08:59:22 +0200

    feat: jquery in assets

--- assets/back/app.js
+++ assets/back/app.js
@@ -1,4 +1,10 @@
-import './scss/main.scss';
+
+global.$ = global.jQuery = $
 
 // start the Stimulus application
 import './../bootstrap';
+
+import './js/collection';
+
+import './scss/main.scss';
+
